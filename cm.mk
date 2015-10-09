b2624f850b12a3cb59cd3e548a627614ad7fb60b
vm03 <vm03@yandex.ru>
2015-10-10 00:27:25 +0300

    Initial jag3gds commit

---
+++ cm.mk
@@ -0,0 +1,27 @@
+# Boot animation
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/g3s-common/device.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_DEVICE := jag3gds
+PRODUCT_RELEASE_NAME := LG G3s
+PRODUCT_NAME := cm_jag3gds
+PRODUCT_BRAND := LG
+PRODUCT_MODEL := G3s
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=jag3gds_global_com \
+    BUILD_FINGERPRINT=lge/jag3gds_global_com/jag3gds:5.1.1/LMY48M/2167285:user/release-keys \
+    PRIVATE_BUILD_DESC="jag3gds_global_com-user 5.0.1 5.1.1 LMY48M 2167285 release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.radio.multisim.config=dsds
+
+$(call inherit-product, vendor/lge/jag3gds/jag3gds-vendor.mk)
